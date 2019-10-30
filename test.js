d8d6ff2899217323d4c3724c3dcf680c12d4b437
Robert Parsons <rmpcoding@gmail.com>
2019-10-30 16:17:47 -0700

    Accessible object now

--- test.js
+++ test.js
@@ -1,23 +1,21 @@
 var apiKey = "Y8vTljiG";
 var queryUrl = "https://api.rescuegroups.org/http/v2.json";
 
-
 // male/female
 // dog/cat
 // radius
 // zipcode
 
-
-var data = {
-  "apikey": apiKey,
-  "objectType": "animals",
-  "objectAction": "publicSearch",
-  "search": {
-    "calcFoundRows": "Yes",
-    "resultStart": 0,
-    "resultLimit": 20,
-    "resultSort": "animalID",
-    "fields": [
+var dataObject = {
+  apikey: "Y8vTljiG",
+  objectType: "animals",
+  objectAction: "publicSearch",
+  search: {
+    calcFoundRows: "Yes",
+    resultStart: 0,
+    resultLimit: 20,
+    resultSort: "animalID",
+    fields: [
       "animalID",
       "animalOrgID",
       "animalName",
@@ -25,72 +23,77 @@ var data = {
       "animalBreed",
       "animalThumbnailUrl"
     ],
-    "filters": [
+    filters: [
       {
-        "fieldName": "animalStatus",
-        "operation": "equals",
-        "criteria": "Available"
+        fieldName: "animalStatus",
+        operation: "equals",
+        criteria: "Available"
       }
     ]
   }
 };
-console.log(data);
 
-var string = JSON.stringify(data);
-console.log(string);
+var dog = "dog"; // will ultimately erase and use dropDownAnimal
+var cat = "cat"; // will ultimately erase and use dropDownAnimal
+var dropDownAnimal = ""; // push dropdown value (dog or cat) into the variable
 
-
-$.ajax({
-url: queryUrl,
-method: "POST",
-contentType: "JSON"
-}).then(function (response) {
-    console.log(response);
-});
-
-// 
-
-
-
-var apiKey= "Y8vTljiG";
-var dataObject = {
-    apikey: "Y8vTljiG",
-    objectType: "animals",
-    objectAction: "publicSearch",
-    search: {
-      calcFoundRows: "Yes",
-      resultStart: 0,
-      resultLimit: 20,
-      resultSort: "animalID",
-      fields: [
-        "animalID",
-        "animalOrgID",
-        "animalName",
-        "animalSpecies",
-        "animalBreed",
-        "animalThumbnailUrl"
-      ],
-      filters: [
-        {
-          fieldName: "animalStatus",
-          operation: "equals",
-          criteria: "Available"
-        }
-      ]
-    }
+// test section
+var apiObject = {
+  apikey: "Y8vTljiG",
+  objectType: "animals",
+  objectAction: "publicSearch",
+  search: {
+    resultStart: 0,
+    resultLimit: 10,
+    resultSort: "animalID",
+    resultOrder: "asc",
+    calcFoundRows: "Yes",
+    fields: [
+      "animalID",
+      "animalOrgID",
+      "animalName",
+      "animalSpecies",
+      "animalBreed",
+      "animalThumbnailUrl"
+    ],
+    filters: [
+      { fieldName: "animalSpecies", 
+        operation: "equals", 
+        criteria: dog },
+      {
+        fieldName: "animalStatus",
+        operation: "equals",
+        criteria: "Available"
+      }
+    ]
+  }
 };
 
-var data = JSON.stringify(dataObject);
+// test section
+// { fieldName: "animalSpecies",
+//   operation: "equals",
+//   criteria: cat
+// }
+// { fieldName: "orgLocationDistance",
+//   operation: "radius",
+//   criteria: 90 },
+//  { fieldName: "orgLocation",
+//     operation: "equals",
+//     locationPostalcode: 90210 }
+
+var data = JSON.stringify(apiObject);
 
 var url = `https://cors-anywhere.herokuapp.com/https://api.rescuegroups.org/http/v2.json/`;
 
-$("#myButton").on("click", function() {
+$("#test-display").on("click", function() {
   $.ajax({
     url: url,
-    method:"POST",
-    data,
+    method: "POST",
+    data
   }).then(function(response) {
+    var petValues = Object.values(response.data)
     console.log(response);
-    
+    console.log(petValues[0]);
+
   });
 });
